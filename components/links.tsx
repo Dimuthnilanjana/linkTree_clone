97cb19e12181b6b40fe1c986073bee6872b68288
Dimuthnilanjana <dimuthnilanjana7@gmail.com>
2025-03-20 22:25:16 +0530

    test

--- components/links.tsx
+++ components/links.tsx
@@ -37,7 +37,7 @@ const links = [
     title: "YouTube Channel",
     url: "https://youtube.com/@dimuth",
     icon: Youtube,
-    style: "outline" //outline
+    style: "glass" //outline
   },
   {
     title: "Contact Me",
